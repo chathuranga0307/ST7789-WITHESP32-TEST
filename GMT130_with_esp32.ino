a2c2e06a4163c3c070696386aebbdb8ebc60f9d5
Chathuranga Adasooriya <38062280+chathuranga0307@users.noreply.github.com>
2022-12-22 22:03:21 +0530

    Update GMT130_with_esp32.ino

--- GMT130_with_esp32.ino
+++ GMT130_with_esp32.ino
@@ -18,6 +18,7 @@ int m = imageW;
 int start = 1;
 unsigned short imageS[54720] = {0}; // edit this screenW * screen H
 
+
 int pos = 0;
 int x = 0;
 int y = 0;
@@ -39,6 +40,7 @@ typedef struct struct_message {
   float c;
   bool d;
 } struct_message;
+
 // Create a structured object
 struct_message myData;
 
@@ -50,16 +52,18 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
-void setup() {
-  Serial.begin(115200);
 
-  //_______________________________
+
+
+void setup() {
+
+  // Set up Serial Monitor
+  Serial.begin(115200);
 
   tft.init();
   tft.setRotation(1);
   tft.setSwapBytes(true);
   tft.fillScreen(TFT_BLACK);
-
   pos = x + imageW * y;
   start = pos;
   m = screenW + pos;
@@ -72,6 +76,7 @@ void setup() {
     }
     imageS[i] = Prof_pic[start];
     start++;
+
   }
   x = x + changeX;
   if (x == imageW - screenW - 1 || x < 0)
@@ -83,11 +88,58 @@ void setup() {
 
   tft.pushImage(0, 0, screenW, screenH, imageS);
 
+
+
+  
+
+  // Set ESP32 as a Wi-Fi Station
   //WiFi.mode(WIFI_STA);
 
+  // Initilize ESP-NOW
+  if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+
+  // Register the send callback
+  esp_now_register_send_cb(OnDataSent);
+
+  // Register peer
+  memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+  peerInfo.channel = 0;
+  peerInfo.encrypt = false;
+
+  // Add peer
+  if (esp_now_add_peer(&peerInfo) != ESP_OK) {
+    Serial.println("Failed to add peer");
+    return;
+  }
 }
 
-
 void loop() {
+  // Generate a random integer
+  int_value = random(1, 20);
 
+  // Use integer to make a new float
+  float_value = 1.3 * int_value;
+
+  // Invert the boolean value
+  bool_value = !bool_value;
+
+  // Format structured data
+  strcpy(myData.a, "Welcome to the Workshop!");
+  myData.b = int_value;
+  myData.c = float_value;
+  myData.d = bool_value;
+
+  // Send message via ESP-NOW
+  esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
+
+  if (result == ESP_OK) {
+    Serial.println("Sending confirmed");
+  }
+  else {
+    Serial.println("Sending error");
+  }
+  delay(2000);
 }
